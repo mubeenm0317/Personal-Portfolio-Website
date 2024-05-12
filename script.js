536fa6f41de40c48f741a13360d38ae7996bb86f
Mubeen Aslam <mubeenm0317@gmail.com>
2024-05-12 13:19:23 +0500

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+leftSection = document.querySelector('.leftSection');
+firstSection = document.querySelector('.firstSection');
+purple = document.querySelector('.purple');
+// leftSection.innerHTML = "Hey I am Web Developer at DEV-TECH";
+
+leftSection.style.fontSize = '3rem';
+leftSection.style.backgroundColor = 'transparent';
+// purple.style.color = 'green';
+
+rightSection = document.querySelector('.rightSection');
+// rightSection.innerHTML = <img src="bg.png" alt="background-image"></img>;
+
+// Set the background color to empty string to unset it
+rightSection.style.backgroundColor = 'transparent'; 
+// rightSection.img.style.width = '30%'; 
+
+document.querySelector('.rightSection img').style.width = '90%';
+document.querySelector('.rightSection img').style.margin = '20px 0 40px 50px';
+
+
+
